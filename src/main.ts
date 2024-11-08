2c516ba9b281e3c7604117d229bed24a42b9cea5
hasina <113416278+hasinae@users.noreply.github.com>
2024-11-08 09:53:57 -0800

    L16

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+
+const button = document.createElement("button");
+button.textContent = "Click me!";
+
+button.addEventListener("click", () => {
+  alert("alert('you clicked the button!')");
+});
+
+document.body.appendChild(button);
